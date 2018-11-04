c5a562831ebc9f628e8d25d4b4f997caa923fa90
Alessandro Paperini <paperini.alessandro@gmail.com>
2018-11-04 18:09:39 +0100

    :sparkles: swagger

--- src/main/java/it/laterale/demo/rest/controllers/DemoController.java
+++ src/main/java/it/laterale/demo/rest/controllers/DemoController.java
@@ -13,23 +13,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import it.laterale.demo.rest.model.Demo;
 import it.laterale.demo.rest.services.DemoService;
 
 @RestController
 @RequestMapping("demos")
+@Api(value = "Demo Api")
 public class DemoController {
 
 	@Autowired
 	DemoService demoService;
 
 	@PostMapping("add")
-	public Demo add(@RequestBody Demo entity) {
+	@ApiOperation(value = "Create", notes = "Add new entity")
+	public Demo add(@ApiParam(name = "entity", value = "The body request") @RequestBody Demo entity) {
 		return this.demoService.add(entity);
 	}
 
 	@DeleteMapping("demo/{id}/delete")
-	public void delete(@PathVariable(name = "id") Long id) {
+	@ApiOperation(value = "Delete", notes = "Delete entity")
+	public void delete(@ApiParam(name = "id", value = "Demo entity ID") @PathVariable(name = "id") Long id) {
 		this.demoService.delete(id);
 	}
 
@@ -39,17 +45,21 @@ public class DemoController {
 	}
 
 	@GetMapping("demo/{id}")
-	public Demo get(@PathVariable(name = "id") Long id) {
+	@ApiOperation(value = "Get one", notes = "Get entity by ID")
+	public Demo get(@ApiParam(name = "id", value = "Demo entity ID") @PathVariable(name = "id") Long id) {
 		return this.demoService.getById(id);
 	}
 
 	@GetMapping("demo")
-	public Demo getByName(@RequestParam(name = "name") String name) {
+	@ApiOperation(value = "Get one", notes = "Get entity by name")
+	public Demo getByName(
+			@ApiParam(name = "name", value = "Demo entity name") @RequestParam(name = "name") String name) {
 		return this.demoService.getByName(name);
 	}
 
 	@PutMapping("demo/update")
-	public Demo update(@RequestBody Demo entity) {
+	@ApiOperation(value = "Update", notes = "Update entity")
+	public Demo update(@ApiParam(name = "entity", value = "The body request") @RequestBody Demo entity) {
 		return this.demoService.update(entity);
 	}
 
